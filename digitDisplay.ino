9c00b79e651607b2c7cb4554e3866a6857212a35
lgdelacruz <lgdelacruz@lgdelacruz.com>
2021-06-01 10:32:35 -0400

    First commit

---
+++ digitDisplay.ino
@@ -0,0 +1,17 @@
+
+int a = 2;
+int b = 3;
+int c = 4;
+int d = 5;
+int e = 6;
+int f = 7;
+int g = 8;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
